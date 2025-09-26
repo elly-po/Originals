ace667340ec8c255adb56937b02cae64396df23d
elly-po <ellypaul400@gmail.com>
2025-09-26 17:16:26 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Define all routes here */}
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/admin" element={<Admin />} />
+
+        {/* IMPORTANT: DO NOT place any routes below this. */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
